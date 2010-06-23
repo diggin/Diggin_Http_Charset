224a0d068721a4daa21a415c77ff6db6cefd0255
kazusuke sasezaki <sasezaki+github@gmail.com>
2010-06-23 22:15:01 +0900

    add LICENSE doc-comment

---
+++ library/Diggin/Http/Response/Charset.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Diggin_Http_Response_Charset
+ * 
+ * a part of this package (Diggin_Http_Response_Charset_Detector_Html) is
+ * borrowed from HTMLScraping
+ * 
+ * @see http://www.rcdtokyo.com/etc/htmlscraping/
+ *
+ * LICENSE: This source file is subject to the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or any later version
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/licenses/lgpl.html
+ * If you did not have a copy of the GNU Lesser General Public License
+ * and are unable to obtain it through the web, please write to
+ * the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * ---------------------------------------------------------------------
+ */
+
+final class Diggin_Http_Response_Charset
+{
+    final private function __construct()
+    {}
+
+    final public static function wrapResponse($response, $url = null)
+    {
+        if ($response instanceof Zend_Http_Response) {
+
+            $headers = $response->getHeaders();
+            if (isset($headers['Content-type'])) {
+                $headers['Content-type'] = trim(preg_replace('/charset=[A-Za-z0-9-_]+;*/i', '', $headers['Content-type']));
+            }
+            
+            require_once 'Diggin/Http/Response/Charset/Wrapper/Zf.php';
+            $response = new Diggin_Http_Response_Charset_Wrapper_Zf($response->getStatus(), 
+                                                $headers,
+                                                $response->getRawBody(),
+                                                $response->getVersion(),
+                                                $response->getMessage());
+            $response->setUrl($url);
+
+            return $response;
+        } else {
+            require_once 'Diggin/Http/Response/Charset/Exception.php';
+            throw new Diggin_Http_Response_Charset_Exception('Unknown Object Type..');
+        }
+    }
+}
---
+++ library/Diggin/Http/Response/Charset/Wrapper/Zf.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Diggin_Http_Response_Charset
+ * 
+ * a part of this package (Diggin_Http_Response_Charset_Detector_Html) is
+ * borrowed from HTMLScraping
+ * 
+ * @see http://www.rcdtokyo.com/etc/htmlscraping/
+ *
+ * LICENSE: This source file is subject to the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or any later version
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/licenses/lgpl.html
+ * If you did not have a copy of the GNU Lesser General Public License
+ * and are unable to obtain it through the web, please write to
+ * the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * ---------------------------------------------------------------------
+ */
+
+class Diggin_Http_Response_Charset_Wrapper_Zf extends Zend_Http_Response 
+{
+    /**
+     * @var Diggin_Http_Response_Charset_Front_EncodeInterface
+     */
+    private $_charsetfront;
+
+    /**
+     * @var string
+     */
+    private $_url;
+
+    public function setCharsetFront(Diggin_Http_Response_Charset_Front_EncodeInterface $charsetfront)
+    {
+        $this->_charsetfront = $chasetfront;
+    }
+
+    public function getCharsetFront()
+    {
+        if (!$this->_charsetfront) {
+            require_once 'Diggin/Http/Response/Charset/Front/UrlRegex.php';
+            $this->_charsetfront = new Diggin_Http_Response_Charset_Front_UrlRegex;
+        }
+
+        return $this->_charsetfront;
+    }
+
+    public function getBody()
+    {
+        $resouce = array('body' => parent::getBody(), 'content-type' => $this->getHeader('content-type'));
+        $document = array('url' => $this->getUrl(), 'resouce' => $resouce);
+        
+        return $this->getCharsetFront()->encode($document);
+    }
+
+    public function setUrl($url)
+    {
+        $this->_url = $url;
+    }
+
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+}
